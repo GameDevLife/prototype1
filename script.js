6ce30bf8e89c9402cef7b2e4faf0cc95cf8d9f09
o0pmitev <pmitev89@gmail.com>
2024-01-05 09:21:46 +0200

    fix: improve animation handling

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = "./assets/shadow_dog.png";
+let playerState = "idle";
 /**
  * Sprite sheet width 6876px it has 12 columns of sprites
  * 6876 / 12 = 573
@@ -18,22 +19,76 @@ playerImage.src = "./assets/shadow_dog.png";
  */
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let maxFrame = 6; // All the frames in a sprite animation (on a single row. Some animation may have more or less)
-let idleAnimationFrames = 6; // 6 here is because the idle animation has 6 frames
 let gameFrame = 0;
 const staggerFrame = 5; // it will slow down animation by the amount of the variable
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: "idle",
+        frames: 7,
+    },
+    {
+        name: "jump",
+        frames: 7,
+    },
+    {
+        name: "fall",
+        frames: 7,
+    },
+    {
+        name: "run",
+        frames: 9,
+    },
+    {
+        name: "dizzy",
+        frames: 11,
+    },
+    {
+        name: "sit",
+        frames: 5,
+    },
+    {
+        name: "roll",
+        frames: 7,
+    },
+    {
+        name: "bite",
+        frames: 7,
+    },
+    {
+        name: "ko",
+        frames: 12,
+    },
+    {
+        name: "getHit",
+        frames: 4,
+    }
+]
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    let position = Math.floor(gameFrame/staggerFrame) % idleAnimationFrames;
-    frameX = position * spriteWidth;
+    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimations[playerState].loc.length;
+    let frameX = position * spriteWidth;
+    let frameY = spriteAnimations[playerState].loc[position].y;
     ctx.drawImage(
         playerImage,// source of the image (the sprite sheet)
-        frameX, // area from the srpite sheet to be cut out on the x axis(width of the sprite) times the frame number on the x  [0 - 11] (12) different frames
-        frameY * spriteHeight, // area from the srpite sheet to be cut out on the y axis(height of the sprite) times the frame number on the y [0 - 9] (10) different frames
+        frameX, // area from the srpite sheet to be cut out on the x axis
+        frameY, // area from the srpite sheet to be cut out on the y axis
         spriteWidth, // 1 srpite width
         spriteHeight, // 1 srpite heigth
         0,// x position on the canvas to be drawnd
