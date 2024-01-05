66c8d67dcaf4144ddefa72ccecb9d0202d7d94a7
o0pmitev <pmitev89@gmail.com>
2024-01-05 09:00:03 +0200

    fix: refactor animation logic

--- script-backup.js
+++ script-backup.js
@@ -21,18 +21,16 @@ const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let maxFrame = 6; // All the frames in a sprite animation (on a single row. Some animation may have more or less)
-let idleAnimationFrames = 6; // 6 here is because the idle animation has 6 frames
 let gameFrame = 0;
-const staggerFrame = 5; // it will slow down animation by the amount of the variable
+const staggerFrame = 5; // it will slow down animation by the amount of the variable 
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-    let position = Math.floor(gameFrame/staggerFrame) % idleAnimationFrames;
-    frameX = position * spriteWidth;
+    // ctx.fillRect(50, 50, 100, 100);
+    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
     ctx.drawImage(
         playerImage,// source of the image (the sprite sheet)
-        frameX, // area from the srpite sheet to be cut out on the x axis(width of the sprite) times the frame number on the x  [0 - 11] (12) different frames
+        frameX * spriteWidth, // area from the srpite sheet to be cut out on the x axis(width of the sprite) times the frame number on the x  [0 - 11] (12) different frames
         frameY * spriteHeight, // area from the srpite sheet to be cut out on the y axis(height of the sprite) times the frame number on the y [0 - 9] (10) different frames
         spriteWidth, // 1 srpite width
         spriteHeight, // 1 srpite heigth
@@ -41,6 +39,19 @@ function animate() {
         spriteWidth, // ??? I believe its a scale(stretch) of the drawnd image (width)
         spriteHeight // ??? I believe its a scale(stretch) of the drawnd image (height)
     );
+    /**
+     * gameFrame is increasing in on each call of requestAnimation frame;
+     * let say gameFrame is 17 and staggerFrame is 5
+     *
+     * gameFrame = 17;
+     * staggerFrame = 5;
+     * 17 % 5 = 2
+     * so every 5 frames(staggerFrame) it will return remainder 0 and it will play the next srpite
+    */
+    if (gameFrame % staggerFrame == 0) {
+        if (frameX < maxFrame) frameX++;
+        else frameX = 0;
+    }
 
     gameFrame++;
 
